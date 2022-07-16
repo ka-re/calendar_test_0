af010f6b353856f27cda3e30cae2dd1594171ee3
ka-re <99371917+ka-re@users.noreply.github.com>
2022-07-16 15:39:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,47 +10,20 @@ const events = [
 		borderColor: "red",// 枠線色
 		editable: true// イベント操作の可否
 	},
-	
+	// 省略
 ];
-
-
-
 
 
 //カレンダー全体の設定
 // IDを取得する
 const elem = document.getElementById("my-calendar");
-
 // FullCalendarオブジェクト
 const calendar = new FullCalendar.Calendar(elem, {
-    //カレンダーのオプションなど
+	initialView: "dayGridMonth",    //initialView:カレンダーの種類
 
-    //initialView:カレンダーの種類
-	initialView: "dayGridMonth",    //dayGridMonth:月単位
-    initialView: "dayGridWeek",     //dayGridWeek:週単位
-    initialView: "timeGridDay",     //timeGridDay:日単位
-
-    //たぶんページを読み込んだ時、最初に表示される日
+    
 	initialDate: "2022-07-16",
-
-
-    //カレンダー上部のタイトルやボタンを設定
-    headerToolbar: {
-        left: false,                //左：なし（なしの場合はfalse）
-        center: "title",            //中央：タイトル
-        right: "today prev,next"    //右：今日へ戻るボタン、先月ボタン、次の月ボタン
-    },    
-
-    //日本語化    
-    locale: 'ja',  
-
-    //週の初めの日を設定
-    firstDay: 0,        //0：日曜始まり、1：月曜始まり    （デフォルトは日曜始まり）
-
-    //上で書いたイベント
 	events: events,
-
-
 	dateClick: (e)=>{
 		console.log("dateClick:", e);
 	},
@@ -62,21 +35,5 @@ const calendar = new FullCalendar.Calendar(elem, {
 			content: e.event.extendedProps.description,
 		});
 	}
-
-
-
-    
 });
-
-
-
-
-
-
-// カレンダーを表示する
-calendar.render();
-
-
-
-
-//End
+calendar.render();// カレンダーを表示する
