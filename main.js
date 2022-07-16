1d3511fed9f7c8260a4d0a06aa83cca319098232
ka-re <99371917+ka-re@users.noreply.github.com>
2022-07-16 15:25:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,71 @@
-// イベント挿入
-const events = [
-	{
-		id: "a",// ユニークID
-		start: "2022-07-16",// イベント開始日
-		end: "",// イベント終了日
-		title: "節分",// イベントのタイトル
-		description: "悪い鬼を追い払い福を招く",// イベントの詳細
-		backgroundColor: "red",// 背景色
-		borderColor: "red",// 枠線色
-		editable: true// イベント操作の可否
-	},
-	// 省略
-];
+// // イベント挿入
+// const events = [
+// 	{
+// 		id: "a",// ユニークID
+// 		start: "2022-07-16",// イベント開始日
+// 		end: "",// イベント終了日
+// 		title: "節分",// イベントのタイトル
+// 		description: "悪い鬼を追い払い福を招く",// イベントの詳細
+// 		backgroundColor: "red",// 背景色
+// 		borderColor: "red",// 枠線色
+// 		editable: true// イベント操作の可否
+// 	},
+// 	// 省略
+// ];
 
 
 
 
 
-//カレンダー全体の設定
-// IDを取得する
-const elem = document.getElementById("my-calendar");
+// //カレンダー全体の設定
+// // IDを取得する
+// const elem = document.getElementById("my-calendar");
 
-// FullCalendarオブジェクト
-const calendar = new FullCalendar.Calendar(elem, {
-    //カレンダーのオプションなど
+// // FullCalendarオブジェクト
+// const calendar = new FullCalendar.Calendar(elem, {
+//     //カレンダーのオプションなど
 
-    //initialView:カレンダーの種類
-	initialView: "dayGridMonth",    //dayGridMonth:月単位
-    initialView: "dayGridWeek",     //dayGridWeek:週単位
-    initialView: "timeGridDay",     //timeGridDay:日単位
+//     //initialView:カレンダーの種類
+// 	initialView: "dayGridMonth",    //dayGridMonth:月単位
+//     initialView: "dayGridWeek",     //dayGridWeek:週単位
+//     initialView: "timeGridDay",     //timeGridDay:日単位
 
-    //たぶんページを読み込んだ時、最初に表示される日
-	initialDate: "2022-07-16",
+//     //たぶんページを読み込んだ時、最初に表示される日
+// 	initialDate: "2022-07-16",
 
-    //日本語化    
-    locale: 'ja',  
+//     //日本語化    
+//     locale: 'ja',  
+
+//     //上で書いたイベント
+// 	events: events,
 
 
-	events: events,
-	dateClick: (e)=>{
-		console.log("dateClick:", e);
-	},
-	eventClick: (e)=>{
-		console.log("eventClick:", e.event.title);
-	},
-	eventDidMount: (e)=>{// カレンダーに配置された時のイベント
-		tippy(e.el, {// TippyでTooltipを設定する
-			content: e.event.extendedProps.description,
-		});
-	}
+// 	dateClick: (e)=>{
+// 		console.log("dateClick:", e);
+// 	},
+// 	eventClick: (e)=>{
+// 		console.log("eventClick:", e.event.title);
+// 	},
+// 	eventDidMount: (e)=>{// カレンダーに配置された時のイベント
+// 		tippy(e.el, {// TippyでTooltipを設定する
+// 			content: e.event.extendedProps.description,
+// 		});
+// 	}
 
 
 
     
-});
+// });
 
 
 
 
 
 
-// カレンダーを表示する
-calendar.render();
+// // カレンダーを表示する
+// calendar.render();
 
 
 
 
-//End
+// //End
