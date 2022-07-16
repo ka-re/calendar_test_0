74848963da5c47fdb9bf7cfb06786a9b364f1aff
ka-re <99371917+ka-re@users.noreply.github.com>
2022-07-16 14:18:57 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const events = [
+	{
+		id: "a",// ユニークID
+		start: "2022-02-02",// イベント開始日
+		end: "",// イベント終了日
+		title: "節分",// イベントのタイトル
+		description: "悪い鬼を追い払い福を招く",// イベントの詳細
+		backgroundColor: "red",// 背景色
+		borderColor: "red",// 枠線色
+		editable: true// イベント操作の可否
+	},
+	// 省略
+];
+
+
+// IDを取得する
+const elem = document.getElementById("my-calendar");
+// FullCalendarオブジェクト
+const calendar = new FullCalendar.Calendar(elem, {
+	initialView: "dayGridMonth",
+	initialDate: "2022-02-13",
+	events: events,
+	dateClick: (e)=>{
+		console.log("dateClick:", e);
+	},
+	eventClick: (e)=>{
+		console.log("eventClick:", e.event.title);
+	},
+	eventDidMount: (e)=>{// カレンダーに配置された時のイベント
+		tippy(e.el, {// TippyでTooltipを設定する
+			content: e.event.extendedProps.description,
+		});
+	}
+});
+calendar.render();// カレンダーを表示する
